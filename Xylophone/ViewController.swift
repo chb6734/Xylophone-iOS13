b3c87fe9bfa957162261d15f2d3d8eb128966b31
aofhdzzz1 <chb6734@gmail.com>
2020-11-24 03:11:11 +0900

    버튼마다 다른 소리 나도록 변경

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        print( sender.currentTitle)
+        
+        //sender.currentTitle은 String?(옵셔널)이기 때문에 !을 통해 Unwrapping 해줌
+        playSound(code: sender.currentTitle!)
+    }
     
-
+    func playSound(code :String) {
+        let url = Bundle.main.url(forResource: code, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
